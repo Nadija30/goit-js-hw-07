d1772fc858b4d77d8cfa7887bfac101b1ae11515
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2023-07-03 16:01:41 +0300

    commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+let instance;
+console.log(galleryItems);
+const container = document.querySelector('.gallery');
+const markup = galleryItems.map(({ preview, original, description }) => 
+`<li class = "gallery__item">
+  <a class = "gallery__link" href = "${original}">
+    <img
+      class = "gallery__image"
+      src = "${preview}"
+      alt = "${description}"
+    />
+  </a>
+</li >`);
+
+container.insertAdjacentHTML('beforeend', markup.join(''));
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
+lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
