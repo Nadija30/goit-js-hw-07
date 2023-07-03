f9580a53363661233a481c37afa66f6af9459dbc
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2023-07-03 15:34:47 +0300

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+let instance;
 console.log(galleryItems);
 const container = document.querySelector('.gallery');
 const markup = galleryItems.map(({ preview, original, description }) => 
@@ -24,21 +24,31 @@ if (!evt.target.classList.contains('gallery__image')) {
 }
  //console.log(evt.target)
     const imgSource = evt.target.dataset.source ?? evt.target.closest('img').dataset.source;
-    console.log(imgSource);
-const instance = basicLightbox.create(`
+  console.log(imgSource);
+
+  instance = basicLightbox.create(`
     <img
-      src = "${imgSource}" 
+      src = "${imgSource}" "width="1280" height="auto"
     />
-`)
+`, {
+    onShow: (instance) => {
+      window.addEventListener('keydown', onEscKeyPress);
+    },
+	
+    onClose: (instance) => {
+      window.removeEventListener('keydown', onEscKeyPress);
+    }
+  }
+  );
     instance.show();
+
+}
+
     function onEscKeyPress(evt) {
 if (evt.code !== 'Escape') {
     return;
 }
 instance.close();
 }
-}
-
-
 
 
